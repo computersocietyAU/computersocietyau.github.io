344fe543cd68fccf414d8948a6b96e2fa8d861e5
Ashmn07 <ashwin.menon202@gmail.com>
2022-08-31 14:37:42 +0530

    Modified Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,10 @@
 import React, { useState, useRef } from 'react';
+import {Link} from '@tanstack/react-location'
 
 const Navbar = () => {
 
   const [navView, setNavView] = useState(true);
   const navElem = useRef(); 
-
-  const navItems = ['Home', 'Events', 'Workshops', 'Blog', 'Our Team'];
-  const navLinks = ['/', '/events', '/workshops', '/blogs', '/team'];
 
   return (
     <nav id='nav-container' className='bg-navbarBg w-full min-h-[12vh] pr-[30px] pl-[20px] fixed z-[10] tracking-[1.5px] shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]'>
@@ -15,14 +13,25 @@ const Navbar = () => {
           <a className="no-underline font-navLogo" href='/'>💻 CSAU</a>
         </div>
         <ul ref={navElem} className={"lg:flex hidden justify-between items-center flex-1 md:flex-[0.5] lg:flex-[0.65] list-none translate-y-0 ease-in-out duration-500" + (!navView ? ' hide translate-y-[-200%]' : '')}>
-          {navItems.map((nav, i) => {
-            let isActive = window.location.pathname === navLinks[i];
-            return <li key={i} className={isActive ? 'active text-navSpecial' : ''}> 
-              <a className="no-underline color-white text-[17.6px] leading-[1.75em] cursor-pointer hover:border-b-[3.5px] hover:border-solid hover:border-navSpecial" 
-                href={navLinks[i]}>
-                {nav}
-              </a>
-            </li>;
+          {[
+            [".", "Home"],
+            ["events", "Events"],
+            ["blogs", "Blog"],
+            ["team", "Team"],
+          ].map(([to, label]) => {
+            return (
+              <div key={to}>
+                <Link
+                  to={to}
+                  getActiveProps={() => ({ className: `text-navSpecial` })}
+                  activeOptions={{
+                    exact: to === ".",
+                  }}
+                >
+                  {label}
+                </Link>
+              </div>
+            );
           })}
         </ul>
         <div id='nav-icon-wrapper' className='flex-1 lg:flex-[0.25] cursor-pointer flex flex-row-reverse'>
@@ -35,12 +44,33 @@ const Navbar = () => {
       </div>
       <div className={"mobile-nav block lg:hidden pb-[15px]" + (!navView ? ' hide hidden' : '')}>
         <ul className='text-center list-none ease-in-out duration-500 '>
-          {navItems.map((nav, i) => {
+        {[
+            [".", "Home"],
+            ["events", "Events"],
+            ["blogs", "Blog"],
+            ["team", "Team"],
+          ].map(([to, label]) => {
+            return (
+              <div key={to} className="my-[5px]">
+                <Link
+                  className='no-underline color-white text-sm sm:text-[15px] md:text-[17.6px] sm:leading-[1.75em] cursor-pointer hover:border-b-[3.5px] hover:border-solid hover:border-navSpecial'
+                  to={to}
+                  getActiveProps={() => ({ className: `text-navSpecial` })}
+                  activeOptions={{
+                    exact: to === ".",
+                  }}
+                >
+                  {label}
+                </Link>
+              </div>
+            );
+          })}
+          {/* {navItems.map((nav, i) => {
             let isActive = window.location.pathname === navLinks[i];
             return <li key={i} className={"my-[5px]" + (isActive ? ' active text-navSpecial' : '')}> 
-              <a className="no-underline color-white text-sm sm:text-[15px] md:text-[17.6px] sm:leading-[1.75em] cursor-pointer hover:border-b-[3.5px] hover:border-solid hover:border-navSpecial" href={navLinks[i]}>{nav}</a>
+              <a className="" href={navLinks[i]}>{nav}</a>
             </li>;
-          })}
+          })} */}
         </ul>
       </div>
     </nav>
