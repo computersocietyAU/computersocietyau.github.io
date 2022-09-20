ae29c214463c29d1981e525e67c0db11ba240e61
Ashmn07 <ashwin.menon202@gmail.com>
2022-09-20 22:56:47 +0530

    Sanity Schema Integrated

---
+++ bisquedolphin/schemas/event.js
@@ -0,0 +1,58 @@
+export default {
+    name: 'event',
+    title: 'Event',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'location',
+        title: 'Location',
+        type: 'string',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'mainImage',
+        title: 'Main image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'date',
+        title: 'Conducted on',
+        type: 'datetime',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'blockContent',
+      },
+    ],
+  
+    preview: {
+      select: {
+        title: 'title',
+        media: 'mainImage',
+      },
+    //   prepare(selection) {
+    //     const {author} = selection
+    //     return Object.assign({}, selection, {
+    //       subtitle: author && `by ${author}`,
+    //     })
+    //   },
+    },
+  }
+  
---
+++ bisquedolphin/schemas/team.js
@@ -0,0 +1,41 @@
+export default {
+    name: 'team',
+    title: 'Team',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'lnurl',
+        title: 'Lnurl',
+        type: 'string',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'name',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+    ],
+    preview: {
+      select: {
+        title: 'name',
+        media: 'image',
+      },
+    },
+  }
+  
