871bc1f3d6d975031f09f512aca2774860611718
Karthikeyan S <skarthikeyan2901@gmail.com>
2022-10-25 11:10:33 +0530

    changed team schema

--- sanity-website-content/schemas/team.js
+++ sanity-website-content/schemas/team.js
@@ -1,56 +1,61 @@
 export default {
-    name: 'team',
-    title: 'Team',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-      },
-      {
-        name: 'lnurl',
-        title: 'Lnurl',
-        type: 'string',
-      },
-      {
-        name: 'designation',
-        title: 'Designation',
-        type: 'string',
-      },
-      {
-        name: 'domain',
-        title: 'Domain',
-        type: 'string',
-      },
-      {
-        name: 'year',
-        title: 'Year',
-        type: 'string',
-      },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {
-          source: 'name',
-          maxLength: 96,
-        },
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: {
-          hotspot: true,
-        },
-      },
-    ],
-    preview: {
-      select: {
-        title: 'name',
-        media: 'image',
+  name: "team",
+  title: "Team",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "lnurl",
+      title: "Lnurl",
+      type: "string",
+    },
+    {
+      name: "designation",
+      title: "Designation",
+      type: "string",
+    },
+    {
+      name: "domain",
+      title: "Domain",
+      type: "string",
+    },
+    {
+      name: "year",
+      title: "Year",
+      type: "string",
+    },
+    {
+      name: "department",
+      title: "Department",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
       },
     },
-  }
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+};
   
