ccdf7fa63f46ad9924e343fc38959d9e9500f0d2
Ashmn07 <ashwin.menon202@gmail.com>
2022-11-03 21:57:41 +0530

    Modifications

--- src/components/Events/EventCards/EventCards.js
+++ src/components/Events/EventCards/EventCards.js
@@ -9,11 +9,11 @@ function EventCards({ coverImage, title, description, date, location, background
     }
 
     return (
-        <div className='w-screen xlg:w-[90vw] h-fit py-[2rem] xlg:py-0 pb-[4rem]  mx-auto mt-[2rem] mb-[3rem] flex-col xlg:flex-row items-center xlg:items-start justify-evenly xlg:justify-start rounded-2xl flex xlg:h-[50vh] relative transition ease-in-out hover:cursor-pointer hover:opacity-90 hover:scale-95 duration-200' style={{ background: background }}>
-            <div className='max-w-72 max-h-max m-auto flex xlg:justify-center xlg:items-center xlg:px-[2rem]'>
+        <div className='w-screen xlg:w-[90vw] min-h-max py-[2rem] xlg:py-0 pb-[4rem]  mx-auto mt-[2rem] mb-[3rem] flex-col xlg:flex-row items-center xlg:items-start justify-evenly xlg:justify-start rounded-2xl flex relative transition ease-in-out hover:cursor-pointer hover:opacity-90 hover:scale-95 duration-200' style={{ background: background }}>
+            <div className='max-w-72 max-h-max m-auto flex xlg:justify-center xlg:items-center xlg:px-[2rem] p-5'>
                 <img src={coverImage} alt="cover" className='w-48 md:w-72' />
             </div>
-            <div className='w-[85%] m-auto xlg:m-0 xlg:block flex flex-col justify-center items-center xlg:w-[65%] pt-4'>
+            <div className='w-[85%] m-auto xlg:m-0 xlg:block flex flex-col justify-center items-center xlg:w-[65%] py-4'>
                 <div className='font-bold xlg:text-3xl text-xl my-4 text-center uppercase'>{title}</div>
                 <div className='font-medium text-[14px] xlg:text-[17px] leading-7 xlg:pr-8 xlg:pl-0 p-x-[0.5rem] text-justify'>{truncate(description,500)}
                     <div className='flex xlg:border-0 xlg:block justify-between border-y-[1px] border-y-[#41e6a680] pt-5'>
