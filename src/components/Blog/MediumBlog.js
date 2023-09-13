c0cdb4e68978ce4f58ab632164d7b4e511e63cab
Gautham-22 <gautham22poy@gmail.com>
2023-09-13 09:02:32 +0530

    adding pb in blogs section

--- src/components/Blog/MediumBlog.js
+++ src/components/Blog/MediumBlog.js
@@ -5,77 +5,76 @@ const handleClick = () => {
 
 }
 
-const MediumBlogCard = ({imageURL,title,body,link}) => (
-  
-    <div className="flex justify-center transition ease-in-out hover:scale-105 duration-200">
-  <div
-    className="glassIco block rounded-lg shadow-lg max-h-[300px]" onClick={handleClick}>
+const MediumBlogCard = ({ imageURL, title, body, link }) => (
+
+  <div className="flex justify-center transition ease-in-out hover:scale-105 duration-200">
+    <div
+      className="glassIco block rounded-lg shadow-lg max-h-[300px]" onClick={handleClick}>
       <img
         className="rounded-t-lg object-cover h-3/5 w-full"
         src={imageURL}
         alt={title} />
-    <div className="p-4">
-      <h5
-        className="mb-4 text-xl text-neutral-800 dark:text-neutral-50">
-        {title}
-      </h5>
-      <div className='flex justify-end mt-[2em]'>
-      {/* <a href={link} target="_blank" rel="noreferrer noopener">
+      <div className="p-4">
+        <h5
+          className="mb-4 text-xl text-neutral-800 dark:text-neutral-50">
+          {title}
+        </h5>
+        <div className='flex justify-end mt-[2em]'>
+          {/* <a href={link} target="_blank" rel="noreferrer noopener">
       <button
         type="button"
         className="inline-block rounded bg-navSpecial px-6 pt-2.5 pb-2 text-sm font-medium leading-normal text-navbarBg"
         data-te-ripple-init
         data-te-ripple-color="light">Read more</button>
       </a> */}
+        </div>
       </div>
     </div>
   </div>
-</div>
 )
 
 function MediumBlog() {
 
-    const { data } = useMatch();
-    console.log(data.blogs)
-    // useEffect(() => {
-    //     getBlogData()
-    // }, [])
-  
-    const router = useRouter()
+  const { data } = useMatch();
+  // useEffect(() => {
+  //     getBlogData()
+  // }, [])
 
-    if(router.pending){
-      return(
-        <div class="flex items-center justify-center space-x-2 animate-pulse h-screen w-screen">
-          <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin text-navSpecial"></div>
-        </div>
-      )
-    }
+  const router = useRouter()
 
-    if(data.blogs.length===0){
-      return(
-        <div className="flex flex-col items-center justify-center h-screen w-screen">
+  if (router.pending) {
+    return (
+      <div class="flex items-center justify-center space-x-2 animate-pulse h-screen w-screen">
+        <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin text-navSpecial"></div>
+      </div>
+    )
+  }
+
+  if (data.blogs.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen w-screen">
         <h1 className="font-semibold text-xl md:text-5xl text-navSpecial">No blogs present! Stay tuned for more updates</h1>
-       </div>
-      )
-    }
+      </div>
+    )
+  }
 
   return (
-    <div className="pt-[7em] px-7">
-         <div className="text-white font-main font-bold text-[20px] mb-[2em] xlg:text-[30px] text-center uppercase flex justify-center">
-          <p className="w-fit border-white border-t-[5px] border-b-[5px]">
-            Blogs
-          </p>
-        </div>
-         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:mx-8 gap-y-3 gap-x-8 h-full">
-          {/* <MediumBlogCard/> */}
-          {/* {blogs?.map()} */}
-          {
-          data.blogs.map(blog=>(
-          <a href={blog.link} target="_blank" rel="noreferrer noopener">
-            <MediumBlogCard imageURL={blog.thumbnail} title={blog.title} link={blog.link} body={blog.content}/>
-          </a>
+    <div className="pt-[7em] px-7 pb-7">
+      <div className="text-white font-main font-bold text-[20px] mb-[2em] xlg:text-[30px] text-center uppercase flex justify-center">
+        <p className="w-fit border-white border-t-[5px] border-b-[5px]">
+          Blogs
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:mx-8 gap-y-7 gap-x-8 h-full">
+        {/* <MediumBlogCard/> */}
+        {/* {blogs?.map()} */}
+        {
+          data.blogs.map(blog => (
+            <a href={blog.link} target="_blank" rel="noreferrer noopener">
+              <MediumBlogCard imageURL={blog.thumbnail} title={blog.title} link={blog.link} body={blog.content} />
+            </a>
           ))}
-        </div>
+      </div>
     </div>
   )
 }
--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -8,37 +8,36 @@ import 'swiper/css/bundle';
 import Footer from '../Footer/Footer';
 
 function Events() {
-  
+
 
   const { data } = useMatch();
-  console.log(data)
-      
+
   const router = useRouter()
 
-    if(router.pending){
-      return(
-        <div class="flex items-center justify-center space-x-2 animate-pulse h-screen w-screen">
-          <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin text-navSpecial"></div>
-        </div>
-      )
-    }
-
-    const changeDateFormat = (date) => {
-      if(!date) return;
-      let convertedDate = date.substring(0, 10);
-      let dateArray = convertedDate.split("-");
-      let year = dateArray[0];
-      let month = dateArray[1];
-      let day = dateArray[2];
-      convertedDate = day + "-" + month + "-" + year;
-      return convertedDate;
-    }
-
+  if (router.pending) {
     return (
-      <div className="events-page">
-                {data?.upcomingEvents?.length!=0?
+      <div class="flex items-center justify-center space-x-2 animate-pulse h-screen w-screen">
+        <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin text-navSpecial"></div>
+      </div>
+    )
+  }
+
+  const changeDateFormat = (date) => {
+    if (!date) return;
+    let convertedDate = date.substring(0, 10);
+    let dateArray = convertedDate.split("-");
+    let year = dateArray[0];
+    let month = dateArray[1];
+    let day = dateArray[2];
+    convertedDate = day + "-" + month + "-" + year;
+    return convertedDate;
+  }
+
+  return (
+    <div className="events-page">
+      {data?.upcomingEvents?.length !== 0 ?
         <div className="section-bg pt-[14vh] h-fit w-full ">
-          
+
 
           <Subheading title="Upcoming Events" />
 
@@ -48,8 +47,8 @@ function Events() {
             slidesPerView={1}
             navigation
             pagination={{ clickable: true }}
-            onSwiper={() => {}}
-            onSlideChange={() => {}}
+            onSwiper={() => { }}
+            onSlideChange={() => { }}
           >
             {data?.upcomingEvents?.map(
               ({ title, mainImage, description, date, location }, index) => (
@@ -68,28 +67,28 @@ function Events() {
               )
             )}
           </Swiper>
-        </div>:<div className='pb-[4rem]'></div>}
-        <div className="pt-[3rem]">
-          <Subheading title="Our Events" />
+        </div> : <div className='pb-[4rem]'></div>}
+      <div className="pt-[3rem]">
+        <Subheading title="Our Events" />
 
-          {data?.events?.map(
-            ({ title, mainImage, description, date, location }, index) => (
-              <EventCards
-                key={index}
-                title={title}
-                coverImage={mainImage.asset.url}
-                description={description[0].children[0].text} // change to block content in sanity
-                date={changeDateFormat(date)}
-                location={location}
-                // buttonText="See pictures ->"
-                background={"rgba(65, 230, 166, 0.3)"}
-              />
-            )
-          )}
-        </div>
-        <Footer/>
+        {data?.events?.map(
+          ({ title, mainImage, description, date, location }, index) => (
+            <EventCards
+              key={index}
+              title={title}
+              coverImage={mainImage.asset.url}
+              description={description[0].children[0].text} // change to block content in sanity
+              date={changeDateFormat(date)}
+              location={location}
+              // buttonText="See pictures ->"
+              background={"rgba(65, 230, 166, 0.3)"}
+            />
+          )
+        )}
       </div>
-    );
+      <Footer />
+    </div>
+  );
 }
 
 export default Events
