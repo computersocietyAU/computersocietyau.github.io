1df6ea610967a98a56313a08a704ef7e2fda6fec
Edumba Vannia Raja <edumbasankaravel513@gmail.com>
2023-09-19 18:42:50 +0530

    yearwise and event card show merged

--- src/components/Events/EventCards/EventCards.js
+++ src/components/Events/EventCards/EventCards.js
@@ -1,33 +1,53 @@
-import React from 'react'
-import CalenderIcon from '../Icons/CalenderIcon';
-import PlaceIcon from '../Icons/PlaceIcon';
+import React from "react";
+import CalenderIcon from "../Icons/CalenderIcon";
+import PlaceIcon from "../Icons/PlaceIcon";
 
-function EventCards({ coverImage, title, description, date, location, background, buttonText }) {
+function EventCards({
+  coverImage,
+  title,
+  description,
+  date,
+  location,
+  background,
+  buttonText,
+}) {
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
-    function truncate(str, n) {
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-    }
-
-    return (
-        <div className='w-screen xlg:w-[90vw] min-h-max py-[2rem] xlg:py-0 pb-[4rem]  mx-auto mt-[2rem] mb-[3rem] flex-col xlg:flex-row items-center xlg:items-start justify-evenly xlg:justify-start rounded-2xl flex relative transition ease-in-out hover:cursor-pointer hover:opacity-90 hover:scale-95 duration-200' style={{ background: background }}>
-            <div className='max-w-72 max-h-max m-auto flex xlg:justify-center xlg:items-center xlg:px-[2rem] p-5'>
-                <img src={coverImage} alt={title} className='w-48 md:w-72' />    //to display title under each poster
-            </div>
-            <div className='w-[85%] m-auto xlg:m-0 xlg:block flex flex-col justify-center items-center xlg:w-[65%] py-4'>
-                <div className='font-bold xlg:text-3xl text-xl my-4 text-center uppercase'>{title}</div>
-                <div className='font-medium text-[14px] xlg:text-[17px] leading-7 xlg:pr-8 xlg:pl-0 p-x-[0.5rem] text-justify'>
-                    <p className="pb-5">{truncate(description,600)}</p>
-                    <div className='flex xlg:border-0 xlg:block justify-between border-y-[1px] border-y-[#41e6a680] py-2 my-2'>
-                        <div className='xlg:text-lg text-sm flex items-center xlg:gap-4 gap-2 text-start m-2.5 uppercase'><CalenderIcon height={30} width={30} /><p>{date}</p></div>
-                        <div className='xlg:text-lg text-sm flex items-center xlg:gap-4 gap-2 text-start m-2.5 uppercase'><PlaceIcon height={30} width={30} /><p>{location}</p></div>
-                    </div>
-                </div>
-                {buttonText?<button className='static xlg:absolute bottom-[2rem] w-[150px] text-[14px] xlg:text-[16px]   xlg:w-fit right-[1rem] mt-[1rem] xlg:mt-0 mr-0 xlg:mr-[3rem] uppercase bg-[#D9D9D9] text-[#192444] rounded-xl py-[10px] px-[15px] hover:bg-[#41e6a7b7] hover:text-[#d9d9d9] '>{buttonText}</button>:null}
-
-            </div>
-
+  return (
+    <div
+      className="w-screen xlg:w-[90vw] min-h-max py-[2rem] xlg:py-0 pb-[4rem]  mx-auto mt-[2rem] mb-[3rem] flex-col xlg:flex-row items-center xlg:items-start justify-evenly xlg:justify-start rounded-2xl flex relative transition ease-in-out hover:cursor-pointer hover:opacity-90 hover:scale-95 duration-200"
+      style={{ background: background }}
+    >
+      <div className="max-w-72 max-h-max m-auto flex xlg:justify-center xlg:items-center xlg:px-[2rem] p-5">
+        <img src={coverImage} alt={title} className="w-48 md:w-72" />
+      </div>
+      <div className="w-[85%] m-auto xlg:m-0 xlg:block flex flex-col justify-center items-center xlg:w-[65%] py-4">
+        <div className="font-bold xlg:text-3xl text-xl my-4 text-center uppercase">
+          {title}
         </div>
-    )
+        <div className="font-medium text-[14px] xlg:text-[17px] leading-7 xlg:pr-8 xlg:pl-0 p-x-[0.5rem] text-justify">
+          <p className="pb-5">{truncate(description, 600)}</p>
+          <div className="flex xlg:border-0 xlg:block justify-between border-y-[1px] border-y-[#41e6a680] py-2 my-2">
+            <div className="xlg:text-lg text-sm flex items-center xlg:gap-4 gap-2 text-start m-2.5 uppercase">
+              <CalenderIcon height={30} width={30} />
+              <p>{date}</p>
+            </div>
+            <div className="xlg:text-lg text-sm flex items-center xlg:gap-4 gap-2 text-start m-2.5 uppercase">
+              <PlaceIcon height={30} width={30} />
+              <p>{location}</p>
+            </div>
+          </div>
+        </div>
+        {buttonText ? (
+          <button className="static xlg:absolute bottom-[2rem] w-[150px] text-[14px] xlg:text-[16px]   xlg:w-fit right-[1rem] mt-[1rem] xlg:mt-0 mr-0 xlg:mr-[3rem] uppercase bg-[#D9D9D9] text-[#192444] rounded-xl py-[10px] px-[15px] hover:bg-[#41e6a7b7] hover:text-[#d9d9d9] ">
+            {buttonText}
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
-export default EventCards
+export default EventCards;
--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,59 +1,68 @@
-import React, {useEffect} from 'react'
-import Subheading from './Subheading/Subheading';
-import EventCards from './EventCards/EventCards';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { useMatch, useRouter } from '@tanstack/react-location';
-import 'swiper/css/bundle';
-import Footer from '../Footer/Footer';
-import 'viewerjs/dist/viewer.css';
-import Viewer from 'viewerjs';
+import React, { useEffect } from "react";
+import Subheading from "./Subheading/Subheading";
+import Yearheading from "./YearHeading/YearHeading";
+import EventCards from "./EventCards/EventCards";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useMatch, useRouter } from "@tanstack/react-location";
+import "swiper/css/bundle";
+import Footer from "../Footer/Footer";
+import "viewerjs/dist/viewer.css";
+import Viewer from "viewerjs";
 
 function Events() {
   //For sliding posters
   let galleryViewer = null;
 
-    useEffect(()=> {
-        if (!galleryViewer) {
-            galleryViewer = new Viewer(
-                document.getElementById('gallery_image_container'),
-                {
-                    title: [4, (coverImage, coverImageData) => `${coverImage.alt}`]
-                }
-            );
+  useEffect(() => {
+    if (!galleryViewer) {
+      galleryViewer = new Viewer(
+        document.getElementById("gallery_image_container"),
+        {
+          title: [4, (coverImage, coverImageData) => `${coverImage.alt}`],
         }
-    },[])
+      );
+    }
+  }, []);
 
   const { data } = useMatch();
-  console.log(data)
-      
-  const router = useRouter()
-
-    if(router.pending){
-      return(
-        <div class="flex items-center justify-center space-x-2 animate-pulse h-screen w-screen">
-          <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin text-navSpecial"></div>
-        </div>
-      )
+  const year_wise_data = data?.events?.reduce((acc, item) => {
+    let d = new Date(item.date);
+    let fullYear = d.getFullYear();
+    if (!acc[fullYear]) {
+      acc[fullYear] = [];
     }
+    acc[fullYear].push(item);
+    return acc;
+  }, {});
+  const sortedYears = Object.keys(year_wise_data).sort().reverse();
+  console.log(data);
 
-    const changeDateFormat = (date) => {
-      if(!date) return;
-      let convertedDate = date.substring(0, 10);
-      let dateArray = convertedDate.split("-");
-      let year = dateArray[0];
-      let month = dateArray[1];
-      let day = dateArray[2];
-      convertedDate = day + "-" + month + "-" + year;
-      return convertedDate;
-    }
+  const router = useRouter();
 
+  if (router.pending) {
     return (
-      <div className="events-page">
-                {data?.upcomingEvents?.length!=0?
-        <div className="section-bg pt-[14vh] h-fit w-full ">
-          
+      <div class="flex items-center justify-center space-x-2 animate-pulse h-screen w-screen">
+        <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin text-navSpecial"></div>
+      </div>
+    );
+  }
 
+  const changeDateFormat = (date) => {
+    if (!date) return;
+    let convertedDate = date.substring(0, 10);
+    let dateArray = convertedDate.split("-");
+    let year = dateArray[0];
+    let month = dateArray[1];
+    let day = dateArray[2];
+    convertedDate = day + "-" + month + "-" + year;
+    return convertedDate;
+  };
+
+  return (
+    <div className="events-page">
+      {data?.upcomingEvents?.length != 0 ? (
+        <div className="section-bg pt-[14vh] h-fit w-full ">
           <Subheading title="Upcoming Events" />
 
           <Swiper
@@ -65,10 +74,38 @@ function Events() {
             onSwiper={() => {}}
             onSlideChange={() => {}}
           >
-            <div id = 'gallery_image_container'>
-            {data?.upcomingEvents?.map(
-              ({ title, mainImage, description, date, location }, index) => (
-                <SwiperSlide key={index}>
+            <div id="gallery_image_container">
+              {data?.upcomingEvents?.map(
+                ({ title, mainImage, description, date, location }, index) => (
+                  <SwiperSlide key={index}>
+                    <EventCards
+                      key={index}
+                      title={title}
+                      coverImage={mainImage.asset.url}
+                      description={description[0].children[0].text} // change to block content in sanity
+                      date={changeDateFormat(date)}
+                      location={location}
+                      buttonText="Register Now !"
+                      background={"rgba(25, 36, 68, 0.6)"}
+                      //onClick={() => handleViewerClick(index)}
+                    />
+                  </SwiperSlide>
+                )
+              )}
+            </div>
+          </Swiper>
+        </div>
+      ) : (
+        <div className="pb-[4rem]"></div>
+      )}
+      <div className="pt-[3rem]">
+        <Subheading title="Our Events" />
+        {sortedYears?.map((year) => (
+          <div>
+            <Yearheading title={year} />
+            <div id="gallery_image_container">
+              {year_wise_data[year]?.map(
+                ({ title, mainImage, description, date, location }, index) => (
                   <EventCards
                     key={index}
                     title={title}
@@ -76,39 +113,19 @@ function Events() {
                     description={description[0].children[0].text} // change to block content in sanity
                     date={changeDateFormat(date)}
                     location={location}
-                    buttonText="Register Now !"
-                    background={"rgba(25, 36, 68, 0.6)"}
-                    //onClick={() => handleViewerClick(index)}
+                    // buttonText="See pictures ->"
+                    background={"rgba(65, 230, 166, 0.3)"}
+                    // onClick={() => handleViewerClick(index)}
                   />
-                </SwiperSlide>
-              )
-            )}
+                )
+              )}
             </div>
-          </Swiper>
-        </div>:<div className='pb-[4rem]'></div>}
-        <div className="pt-[3rem]">
-          <Subheading title="Our Events" />
-              <div id = 'gallery_image_container'>
-          {data?.events?.map(
-            ({ title, mainImage, description, date, location }, index) => (
-              <EventCards
-                key={index}
-                title={title}
-                coverImage={mainImage.asset.url}
-                description={description[0].children[0].text} // change to block content in sanity
-                date={changeDateFormat(date)}
-                location={location}
-                // buttonText="See pictures ->"
-                background={"rgba(65, 230, 166, 0.3)"}
-               // onClick={() => handleViewerClick(index)}
-              />
-            )
-          )}
           </div>
-        </div>
-        <Footer/>
+        ))}
       </div>
-    );
+      <Footer />
+    </div>
+  );
 }
 
-export default Events
+export default Events;
